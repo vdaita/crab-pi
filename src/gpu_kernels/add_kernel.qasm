1d927a67455c16e820dc9630e2133e6c7e30ddda
V Daita <vdaita@gmail.com>
2026-02-20 23:14:09 -0800

    get the full row

--- src/gpu_kernels/add_kernel.qasm
+++ src/gpu_kernels/add_kernel.qasm
@@ -1,5 +1,7 @@
 .include "vc4.qinc"
 
+    
+
 mov r0, unif # r0 <- src addresses
 mov r1, unif # r1 <- src address 2
 mov r2, unif # r2 <- output address
@@ -9,27 +11,47 @@ mov vr_setup, vdr_setup_0(0, 16, 4, vdr_h32(1, 0, 0))
 mov vr_addr, r0 # launch dma load
 mov -, vr_wait
 
-mov vr_setup, vpm_setup(1, 1, h32(0)) # read 16 rows, increment by 1 after each read, start at vpm coord 0,0
-mov ra0, vpm
+mov vr_setup, vdr_setup_1(64)
+mov vr_setup, vdr_setup_0(0, 16, 4, vdr_h32(1, 4, 0))
+mov vr_addr, r1 # launch dma load
 mov -, vr_wait
 
 # ----
 
-mov vr_setup, vdr_setup_1(64)
-mov vr_setup, vdr_setup_0(0, 16, 4, vdr_h32(1, 1, 0))
-mov vr_addr, r1 # launch dma load
+mov vr_setup, vpm_setup(1, 1, h32(0)) # read 16 rows, increment by 1 after each read, start at vpm coord 0,0
+mov ra0, vpm
 mov -, vr_wait
 
-mov vr_setup, vpm_setup(16, 1, h32(1)) # read 16 rows, increment by 1 after each write, start at vpm coord 0,0
+mov vr_setup, vpm_setup(1, 1, h32(4)) # read 16 rows, increment by 1 after each write, start at vpm coord 0,0
 mov rb0, vpm
 mov -, vr_wait
 
-mov vw_setup, vpm_setup(16, 1, h32(2))
+mov vw_setup, vpm_setup(1, 1, h32(8))
 add r3, ra0, rb0
 mov vpm, r3
 mov -, vw_wait
 
-mov vw_setup, vdw_setup_0(16, 4, dma_h32(2, 0))
+# ---
+
+# mov vr_setup, vpm_setup(16, 2, h32(0)) # read 16 rows, increment by 1 after each read, start at vpm coord 0,0
+# mov ra0, vpm
+# mov -, vr_wait
+
+# mov vr_setup, vpm_setup(16, 2, h32(4)) # read 16 rows, increment by 1 after each write, start at vpm coord 0,0
+# mov rb0, vpm
+# mov -, vr_wait
+
+# mov vw_setup, vpm_setup(16, 2, h32(8))
+# add r3, ra0, rb0
+# mov vpm, r3
+# mov -, vw_wait
+
+# ---
+
+
+# ---
+
+mov vw_setup, vdw_setup_0(1, 16, dma_h32(8, 0))
 mov vw_addr, r2 # write to the destination address
 mov -, vw_wait
 
--- src/programs/gpu_test.rs
+++ src/programs/gpu_test.rs
@@ -1,5 +1,5 @@
 use crate::gpu::{GpuKernel, DEADBEEF_GPU_CODE, ADD_KERNEL_CODE};
-use crate::println;
+use crate::{print, println};
 
 pub fn deadbeef_kernel() {
     unsafe {
@@ -21,24 +21,33 @@ pub fn add_kernel() {
         let gpu_ptr = GpuKernel::init(ADD_KERNEL_CODE);
         let gpu = &mut *gpu_ptr;
 
-        let a = [0, 1, 2, 3];
-        let b = [6, 7, 8, 9];
-        let len = core::mem::size_of_val(&a);
+        let a: [u32; 64] = core::array::from_fn(|i| i as u32);
+        let b: [u32; 64] = core::array::from_fn(|i| (i as u32) + 6);
+        let n = a.len(); // copy_nonoverlapping expects element count
 
-        core::ptr::copy_nonoverlapping(a.as_ptr(), gpu.data[0][0].as_mut_ptr(), len);
-        core::ptr::copy_nonoverlapping(b.as_ptr(), gpu.data[0][1].as_mut_ptr(), len);
+        core::ptr::copy_nonoverlapping(a.as_ptr(), gpu.data[0][0].as_mut_ptr(), n);
+        core::ptr::copy_nonoverlapping(b.as_ptr(), gpu.data[0][1].as_mut_ptr(), n);
 
-        for i in 0..3 {
-            println!("Memory in array {} before running code: {} {} {} {}", i, gpu.data[0][i][0], gpu.data[0][i][1], gpu.data[0][i][2], gpu.data[0][i][3]);
+        print!("Before: out[0..16] =");
+        for i in 0..16 {
+            print!(" {}", gpu.data[0][2][i]);
         }
+        println!("");
 
         gpu.execute();
 
-        for i in 0..3 {
-            println!("Memory in array {} before running code: {} {} {} {}", i, gpu.data[0][i][0], gpu.data[0][i][1], gpu.data[0][i][2], gpu.data[0][i][3]);
+        print!("After: out[0..16] =");
+        for i in 0..16 {
+            print!(" {}", gpu.data[0][2][i]);
         }
-        gpu.release();
+        println!("");
+        println!(
+            "Check: out[63] = {} (expected {})",
+            gpu.data[0][2][63],
+            a[63] + b[63]
+        );
 
+        gpu.release();
         println!("Finished releasing test_gpu");
     }
 }
